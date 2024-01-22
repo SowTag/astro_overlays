9b2bf862001b2e055669e3b85ed56aa533f5fefe
Maddock <sowtag@gmail.com>
2024-01-22 02:33:47 -0300

    Add files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Astro overlays
+Various overlays to enable astro-specific functionality on SnowAOSP and any AOSP-based ROM/GSI.
---
+++ overlay.mk
@@ -0,0 +1,7 @@
+PRODUCT_PACKAGES += \
+	astro-overlay-framework \
+	astro-overlay-systemui \
+	astro-overlay-settings \
+	astro-overlay-lte \
+
+# maddock.overlay.astro.[name]
